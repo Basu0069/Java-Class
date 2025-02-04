ebc7bcb8894240152f257662af8d1112e4bbc1df
Basu Slathia <basuslathia01@gmail.com>
2025-02-04 10:56:20 +0530

    methods

---
+++ _022_method.java
@@ -0,0 +1,15 @@
+// wap to call a method in main method and print hello world in statement
+
+
+
+
+public class _022_method {
+    public static void HelloWorld() {
+        System.out.println("Hello,World!");
+        
+    }
+    public static void main(String[] args) {
+        HelloWorld();
+
+    }
+}
